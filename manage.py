f01bc4b8217d6ba1e1879ff8d08f602a0aa15452
金鹏 <jinpeng@163.com>
2018-10-19 18:32:39 +0800

    导入模型类&创建数据库&导入测试数据

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import create_app, db
+# 需要将表模型导入manage
+from info import create_app, db, models
 
 # 通过指定配置的名字创建app
 app = create_app('development')
@@ -8,7 +9,7 @@ manager = Manager(app)
 # 将app与db关联
 Migrate(app, db)
 # 将迁移命令添加到manager中
-manager.add_command(MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
